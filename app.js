2e84e1d895512e332afd96cd01dab050b1130a4a
HUGO LOPES DE SOUZA <24122289@CFP127ALUNO.LOCAL>
2025-04-03 12:17:04 -0300

    app modificado e pasta para consumo da api e estilização adicionada

--- app.js
+++ app.js
@@ -4,81 +4,81 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((request, response, next) => { 
-    response.header('Access-Control-Allow-Origin', '*')
-    response.header('Access-Crontol-Allow-Methods', 'GET')
+app.use((request, response, next) => {
+	response.header('Access-Control-Allow-Origin', '*')
+	response.header('Access-Crontol-Allow-Methods', 'GET')
 
-    app.use(cors())
+	app.use(cors())
 
-    next()
+	next()
 })
 
 let whatsapp = require('./funcoes')
 
-app.get('/v1/whatsapp/data/:numero', async function(request, response) {
-    let uf = request.params.numero;
-    let dados = whatsapp.getPersonalDate(uf);
-  
-    if (dados) { 
-      response.status(200).json(dados);
-    } else {
-      response.status(404).json({ 'status': 404, 'message': 'Dados não localizados.' })
-    }
-  })
+app.get('/v1/whatsapp/data/:numero', async function (request, response) {
+	let uf = request.params.numero
+	let dados = whatsapp.getPersonalDate(uf)
 
-  app.get('/v1/whatsapp/data2/:numero', async function(request, response) {
-    let uf = request.params.numero
-    let dados = whatsapp.getUserAccountData(uf)
-  
-    if (dados) { 
-      response.status(200).json(dados)
-    } else {
-      response.status(404).json({ 'status': 404, 'message': 'Dados não localizados.' })
-    }
+	if (dados) {
+		response.status(200).json(dados)
+	} else {
+		response.status(404).json({ status: 404, message: 'Dados não localizados.' })
+	}
 })
 
-app.get('/v1/whatsapp/data3/:numero', async function(request, response) {
-    let uf = request.params.numero
-    let dados = whatsapp.getContactsData(uf)
-  
-    if (dados) { 
-      response.status(200).json(dados)
-    } else {
-      response.status(404).json({ 'status': 404, 'message': 'Dados não localizados.' })
-    }
-  })
+app.get('/v1/whatsapp/data2/:numero', async function (request, response) {
+	let uf = request.params.numero
+	let dados = whatsapp.getUserAccountData(uf)
 
-app.get('/v1/whatsapp/data4/:numero', async function(request, response) {
-  let uf = request.params.numero
-  let dados = whatsapp.getUserMessages(uf)
-
-  if (dados) { 
-    response.status(200).json(dados)
-  } else {
-    response.status(404).json({ 'status': 404, 'message': 'Dados não localizados.' })
-  }
+	if (dados) {
+		response.status(200).json(dados)
+	} else {
+		response.status(404).json({ status: 404, message: 'Dados não localizados.' })
+	}
 })
 
-app.get('/v1/whatsapp/filtro', async function(request, response) {
-  let uf = request.query.numero
-  let uf2 = request.query.contato
+app.get('/v1/whatsapp/data3/:numero', async function (request, response) {
+	let uf = request.params.numero
+	let dados = whatsapp.getContactsData(uf)
 
-  let uf3 = request.query.number
-  let uf4 = request.query.contact
-  let uf5 = request.query.chave
-  
-  let dados = whatsapp.getUserContacts(uf, uf2)
-  let data = whatsapp.getKeyWord(uf3, uf4, uf5)
-
-  if (dados) { 
-    response.status(200).json(dados)
-  } else if (data) {
-    response.status(200).json(data)
-  } else {
-    response.status(404).json({ 'status': 404, 'message': 'Dados não localizados.' })
-  }
+	if (dados) {
+		response.status(200).json(dados)
+	} else {
+		response.status(404).json({ status: 404, message: 'Dados não localizados.' })
+	}
 })
-  
-app.listen('8080', function() {
-  console.log('API aguardando requisições...')
+
+app.get('/v1/whatsapp/data4/:numero', async function (request, response) {
+	let uf = request.params.numero
+	let dados = whatsapp.getUserMessages(uf)
+
+	if (dados) {
+		response.status(200).json(dados)
+	} else {
+		response.status(404).json({ status: 404, message: 'Dados não localizados.' })
+	}
+})
+
+app.get('/v1/whatsapp/filtro', async function (request, response) {
+	let uf = request.query.numero
+	let uf2 = request.query.contato
+
+	let uf3 = request.query.number
+	let uf4 = request.query.contact
+	let uf5 = request.query.chave
+
+	let dados = whatsapp.getUserContacts(uf, uf2)
+	let data = whatsapp.getKeyWord(uf3, uf4, uf5)
+
+	if (dados) {
+		response.status(200).json(dados)
+	} else if (data) {
+		response.status(200).json(data)
+	} else {
+		response.status(404).json({ status: 404, message: 'Dados não localizados.' })
+	}
+})
+
+app.listen('8080', function () {
+	console.log('API aguardando requisições...')
 })
