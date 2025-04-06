705095b7bcc877543538267bcb1b1a58240048c1
huguinz <hugolopessouza58@gmail.com>
2025-04-05 21:30:02 -0300

    feat: adicionada funcionalidade de pesquisar por um contato em especifico na barra de pesquisa. Melhorada a manipulação de contatos e correção na exibição do nome

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,15 +1,49 @@
 'use strict'
 
+const nameContact = document.getElementById('search_contact')
+const contacts = document.getElementById('contacts')
+
 const showContacts = async () => {
 	try {
-		const contacts = document.getElementById('contacts')
 		const url = 'http://localhost:8080/v1/whatsapp/data3/11987876567'
 		const response = await fetch(url)
-		const data = await response.json()
-		const object = data.contatos
-		console.log(data)
 
-		object.forEach((item) => {
+		if (response.status === 200) {
+			const data = await response.json()
+			const object = data.contatos
+
+			object.forEach((item) => {
+				const createContacts = document.createElement('div')
+				const listContacts = document.createElement('h2')
+				const imageProfile = document.createElement('img')
+				imageProfile.src = '../img/teste.png'
+				imageProfile.alt = 'profile image'
+				imageProfile.classList.add('profile_image')
+
+				listContacts.textContent = item.nome
+				createContacts.appendChild(imageProfile)
+				createContacts.appendChild(listContacts)
+				contacts.appendChild(createContacts)
+			})
+		} else {
+			alert('erro ao buscar dados dos contatos!')
+		}
+	} catch (error) {
+		console.log(error)
+		alert('error internal server!!')
+	}
+}
+
+showContacts()
+
+const searchContacts = async (name) => {
+	const url = `http://localhost:8080/v1/whatsapp/filtro/?numero=11987876567&contato=${name}`
+	const response = await fetch(url)
+
+	if (response.status === 200) {
+		const data = await response.json()
+		contacts.replaceChildren('')
+		data.contato.forEach((item) => {
 			const createContacts = document.createElement('div')
 			const listContacts = document.createElement('h2')
 			const imageProfile = document.createElement('img')
@@ -17,15 +51,18 @@ const showContacts = async () => {
 			imageProfile.alt = 'profile image'
 			imageProfile.classList.add('profile_image')
 
-			listContacts.textContent = item.nome
+			listContacts.textContent = item.nome_contato
 			createContacts.appendChild(imageProfile)
 			createContacts.appendChild(listContacts)
 			contacts.appendChild(createContacts)
 		})
-	} catch (error) {
-		alert('error internal server!')
-		console.log(error)
+	} else {
+		console.log('usuario não encontrado')
 	}
 }
 
-showContacts()
+nameContact.addEventListener('keydown', (event) => {
+	if (event.key == 'Enter') {
+		return searchContacts(nameContact.value)
+	}
+})
